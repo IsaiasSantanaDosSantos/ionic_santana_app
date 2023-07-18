e6a83f34ebeb6b52ea6752255dab86dd284b39f4
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-07-17 21:32:02 -0300

    creates all of the pages end your route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,58 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: 'Inbox',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    path: "Inbox",
+    redirectTo: "folder/Inbox",
+    pathMatch: "full",
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: "folder/:id",
+    loadChildren: () =>
+      import("./folder/folder.module").then((m) => m.FolderPageModule),
   },
   {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    path: "home",
+    loadChildren: () =>
+      import("./pages/home/home.module").then((m) => m.HomePageModule),
   },
   {
-    path: '',
-    loadChildren: () => import('./pages/splash/splash.module').then( m => m.SplashPageModule)
-  }
+    path: "",
+    loadChildren: () =>
+      import("./pages/splash/splash.module").then((m) => m.SplashPageModule),
+  },
+  {
+    path: "about",
+    loadChildren: () =>
+      import("./pages/about/about.module").then((m) => m.AboutPageModule),
+  },
+  {
+    path: 'services',
+    loadChildren: () => import('./pages/services/services.module').then( m => m.ServicesPageModule)
+  },
+  {
+    path: 'resume',
+    loadChildren: () => import('./pages/resume/resume.module').then( m => m.ResumePageModule)
+  },
+  {
+    path: 'portfolio',
+    loadChildren: () => import('./pages/portfolio/portfolio.module').then( m => m.PortfolioPageModule)
+  },
+  {
+    path: 'references',
+    loadChildren: () => import('./pages/references/references.module').then( m => m.ReferencesPageModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./pages/contact/contact.module').then( m => m.ContactPageModule)
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Home', url: '/home', icon: 'home' },
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: "Home", url: "/home", icon: "home" },
+    { title: "About", url: "/about", icon: "people" },
+    { title: "Services", url: "/services", icon: "code-working" },
+    { title: "Resume", url: "/resume", icon: "document" },
+    { title: "Portfolio", url: "/portfolio", icon: "newspaper" },
+    { title: "References", url: "/references", icon: "ribbon" },
+    { title: "Contact", url: "/contact", icon: "chatbubbles" },
+    // { title: "Inbox", url: "/folder/inbox", icon: "mail" },
+    // { title: "Outbox", url: "/folder/outbox", icon: "paper-plane" },
+    // { title: "Favorites", url: "/folder/favorites", icon: "heart" },
+    // { title: "Archived", url: "/folder/archived", icon: "archive" },
+    // { title: "Trash", url: "/folder/trash", icon: "trash" },
+    // { title: "Spam", url: "/folder/spam", icon: "warning" },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ["Family", "Friends", "Notes", "Work", "Travel", "Reminders"];
   constructor() {}
 }
