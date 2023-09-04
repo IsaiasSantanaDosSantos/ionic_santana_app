d06325780fffcb3683bc66d26d9a7db5f5437c97
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-09-04 18:08:08 -0300

    create new android icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'santana-app',
+  appName: 'Santana App',
   webDir: 'www',
   server: {
     androidScheme: 'https'
